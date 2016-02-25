883062aeb30ac0d48d175f0a791cf668a9d240a9
Rails RAghav Trial <raghav_anand98@yahoo.com>
2016-02-25 05:54:21 +0000

    Style errors and messages v2

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -13,7 +13,7 @@ class ArticlesController < ApplicationController
     @article.destroy
     
     redirect_to articles_path
-    flash[:notice] = "Article was successfully deleted"
+    flash[:danger] = "Article was successfully deleted"
   end
   
   def edit
@@ -22,7 +22,7 @@ class ArticlesController < ApplicationController
   
   def update
     if @article.update(article_params)
-      flash[:notice] = "Article was successfully edited"
+      flash[:success] = "Article was successfully edited"
       redirect_to article_path(@article)
     else
       render 'edit'
@@ -33,7 +33,7 @@ class ArticlesController < ApplicationController
   def create
     @article = Article.new(article_params)
     if @article.save
-      flash[:notice] = "Article was successfully created"
+      flash[:success] = "Article was successfully created"
       redirect_to article_path(@article)
     else
       render 'new'
