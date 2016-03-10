478bc818e19416cac415340459fbf522bb45e7cc
Rails RAghav Trial <raghav_anand98@yahoo.com>
2016-03-10 09:07:20 +0000

    Added pagination to all views

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,7 +6,7 @@ class ArticlesController < ApplicationController
   end
   
   def index
-    @articles = Article.all
+    @articles = Article.paginate(page: params[:page], per_page: 3)
   end
   
   def destroy
@@ -31,7 +31,7 @@ class ArticlesController < ApplicationController
   end
   
   def create
-    debugger
+
     @article = Article.new(article_params)
     @article.user = User.first
     if @article.save
